87c4721f244d7c0dcf3e65530748cae08cf5706b
Apoorva Garg <apoorva.garg@prontoitlabs.com>
2019-06-28 22:17:55 +0530

    TypeRace

---
+++ src/components/TypeRace/TypeRace.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import classes from './Typerace.module.css';
+
+const defaultValues = {
+    countdown: 105
+};
+
+class TypeRace extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            wordcount: 0,
+            quoteTyped: '',
+            wrongChars: 0,
+            inputValue: '',
+            finished: false,
+            wpm: 0,
+            ...defaultValues
+        };
+    }
+
+
+    onKeyDown(e) {
+        const { quoteTyped, wrongChars } = this.state;
+
+        if(wrongChars === 0){
+            this.setState({
+                quoteTyped: quoteTyped.substring(0, quoteTyped.length - 1),
+            });
+        }else {
+            this.setState({
+                wrongChars: (wrongChars - 1 <= 0) ? 0 : wrongChars - 1
+            });
+        }
+    }
+
+    onKeyPress(e) {
+
+        const { quote } = this.props
+        
+        const {
+            quoteTyped, wrongChars, wordcount, countdown
+        } =  this.state;
+
+        const char = String.fromCharCode(e.charCode);
+
+        const quoteIndex = quoteTyped.length;
+
+        if(quoteIndex + wrongChars > quote.length) return;
+
+        if(quoteIndex === quote.length) {
+                this.setState({
+                    finished: true
+                });
+        }
+
+        if(char === ' '){
+            this.setState({
+                wordcount: wordcount + 1
+            })
+        }
+
+        if(char !== quote[quoteIndex]) {
+            this.setState({
+                wrongChars: wrongChars + 1
+            });
+        } else {
+            this.setState({
+                quoteTyped: `${quoteTyped}${char}`,
+                wpm: Math.round(wordcount / ( countdown / 60))
+            });
+        }
+
+    }
+
+    clearlyTyped() {
+        this.setState({
+            inputValue: '',
+            quoteTyped: '',
+            wrongChars: 0
+        });
+    }
+
+    handleChange(e) {
+        const { value } = e.target;
+
+        if(!value) {
+            this.clearlyTyped();
+        }
+        else{
+            this.setState({inputValue: value});
+        }
+    }
+
+    _status() {
+        const { countdown, wpm } = this.state;
+        const timer = moment.utc(countdown * 1000 ).format('hh:mm:ss');
+
+        return (
+            <h4 className={classes.status_bar}>
+                <div>{timer}</div>
+                <div>{`${wpm} wpm`}</div>
+            </h4>
+        );
+    }
+
+    _quote() {
+        const {quote} = this.props;
+        const {
+            quoteTyped, wrongChars
+        } = this.state;
+
+        const quoteLength = quoteTyped.length + wrongChars;
+        const typedWrong = wrongChars > 0 ? quote.substring(quoteTyped.length, quoteTyped.length + wrongChars) : '';
+
+        return (
+            <p>
+                <span className={classes.quote_typed}>{`${quoteTyped}`}</span>
+                <span className={classes.quote_typed_wrong}>{typedWrong}</span>
+                <span>{`${quote.substring(quoteLength)}`}</span>
+            </p>
+        );
+    }
+
+
+    render() {
+        const {quote} = this.props;
+
+        const {inputValue} = this.state;
+
+        const Quote = () => this._quote();
+        const Status = () => this._status();
+
+        return(
+            <div>
+                <Status />
+                <Quote />
+                <input 
+                    ref = {(ref) => { this.input = ref;}}
+                    value={inputValue}
+                    className={classes.input_race}
+                    onKeyPress={e => this.onKeyPress(e)}
+                    onKeyDown = {e => this.onKeyDown(e)}
+                    onChange = {value => this.handleChange(value)}
+                    maxLength = {quote.length}
+                />
+            </div>
+        );
+    }
+}
+
+export default TypeRace;
+
---
+++ src/pages/Race.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import TypeRace from '../components/TypeRace/TypeRace';
+
+const race = () => {
+        return(
+            <div>
+                <TypeRace quote = "Looking back, I saw that for my whole conscious life I had not understood either myself or my strivings. What had seemed for so long to be beneficial now turned out in actuality to be fatal, and I had been striving to go in the opposite direction to that which was truly necessary to me." />
+            </div>
+        );
+    }
+
+export default race;
